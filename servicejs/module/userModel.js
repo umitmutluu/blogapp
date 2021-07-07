eda55acd7258a56fc91a0b32cc5d26c20cea93e3
umitmutlurize <umitmutlurize@gmail.com>
2021-07-07 22:49:26 +0300

    userModel created

---
+++ servicejs/module/userModel.js
@@ -0,0 +1,41 @@
+const mongoose =require('mongoose');
+const Schema=mongoose.Schema;
+const createError=require('http-errors');
+const bcrypt=require('bcrypt');
+
+
+
+const userSchema= new Schema({
+    name:{
+        type:String,
+        required:true,
+        minlength:3,
+        trim:true,
+        maxlength:30
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 30,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true
+
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
+},{collection:'Users',timestamps:tue});
+
+const User=mongoose.model('User',userSchema);
+module.exports=User;
