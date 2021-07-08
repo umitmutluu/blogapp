23e5957ba3c920562b632711bfb2743502c40052
umitmutlurize <umitmutlurize@gmail.com>
2021-07-08 23:33:06 +0300

    userModel added

---
+++ lib/models/userModel.dart
@@ -0,0 +1,57 @@
+// To parse this JSON data, do
+//
+//     final user = userFromJson(jsonString);
+
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
+class User {
+  User({
+    this.id,
+    this.name,
+    this.username,
+    this.email,
+    this.password,
+    this.createdAt,
+    this.updatedAt,
+    this.v,
+  });
+
+  String? id;
+  String? name;
+  String? username;
+  String? email;
+  String? password;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+  int? v;
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        id: json["_id"] == null ? null : json["_id"],
+        name: json["name"] == null ? null : json["name"],
+        username: json["username"] == null ? null : json["username"],
+        email: json["email"] == null ? null : json["email"],
+        password: json["password"] == null ? null : json["password"],
+        createdAt: json["createdAt"] == null
+            ? null
+            : DateTime.parse(json["createdAt"]),
+        updatedAt: json["updatedAt"] == null
+            ? null
+            : DateTime.parse(json["updatedAt"]),
+        v: json["__v"] == null ? null : json["__v"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_id": id == null ? null : id,
+        "name": name == null ? null : name,
+        "username": username == null ? null : username,
+        "email": email == null ? null : email,
+        "password": password == null ? null : password,
+        "createdAt": createdAt == null ? null : createdAt!.toIso8601String(),
+        "updatedAt": updatedAt == null ? null : updatedAt!.toIso8601String(),
+        "__v": v == null ? null : v,
+      };
+}
